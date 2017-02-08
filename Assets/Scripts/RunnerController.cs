8a28bb31a06fa4392a1ecde085aed5db4430749a
David Draginic <daky444@gmail.com>
2017-02-08 17:09:56 +0100

    Initial commit

---
+++ Assets/Scripts/RunnerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunnerController : MonoBehaviour {
+
+	public float jumpForce = 500f;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp ("Jump")) {
+			rb2d.AddForce(transform.up * jumpForce);
+		}
+	}
+}
