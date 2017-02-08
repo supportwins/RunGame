3d924930d5031242656b856f19303997048b8f3a
David Draginic <daky444@gmail.com>
2017-02-08 17:37:14 +0100

    Added Runner Animations

--- Assets/Scripts/RunnerController.cs
+++ Assets/Scripts/RunnerController.cs
@@ -6,10 +6,13 @@ public class RunnerController : MonoBehaviour {
 	public float jumpForce = 500f;
 
 	private Rigidbody2D rb2d;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -17,5 +20,7 @@ public class RunnerController : MonoBehaviour {
 		if (Input.GetButtonUp ("Jump")) {
 			rb2d.AddForce(transform.up * jumpForce);
 		}
+
+		anim.SetFloat ("vVelocity", rb2d.velocity.y);
 	}
 }
